91347a4afc44ffd06f929294ce0846628bc36813
jfornqz <42958376+jfornqz@users.noreply.github.com>
2019-05-29 11:07:15 +0700

    Add files via upload

---
+++ microphone_sensor_basic/microphone_sensor_basic.ino
@@ -0,0 +1,8 @@
+void setup() {
+Serial.begin(9600);                 // เปิดพอร์ตอนุกรมตั้งค่าอัตราข้อมูลเป็น 9600 bps
+}
+loop() {
+int sensorValue = analogRead(A0);   //ตั้งตัวแปล sensorValue ไว้เก็บค่าที่ได้จากการอ่านค่าของinput analog จากขา A0
+Serial.println(sensorValue);        // ปริ๊นค่าที่ได้
+delay(100);                         //ตั้งดีเลย์ 0.1 วินาที
+}                                   
