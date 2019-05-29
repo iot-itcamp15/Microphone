54ecc0bdba72e2ae1df9b0a3a5b0f8b67a43daa3
jfornqz <42958376+jfornqz@users.noreply.github.com>
2019-05-29 15:03:43 +0700

    Update microphone_sensor_basic.ino

--- microphone_sensor_basic/microphone_sensor_basic.ino
+++ microphone_sensor_basic/microphone_sensor_basic.ino
@@ -1,8 +1,9 @@
 void setup() {
-Serial.begin(9600);                 // เปิดพอร์ตอนุกรมตั้งค่าอัตราข้อมูลเป็น 9600 bps
+  pinMode(A0, INPUT);                 // กำหนด A0 เป็น input
+  Serial.begin(9600);                 // เปิดพอร์ตอนุกรมตั้งค่าอัตราข้อมูลเป็น 9600 bps
 }
 loop() {
-int sensorValue = analogRead(A0);   //ตั้งตัวแปล sensorValue ไว้เก็บค่าที่ได้จากการอ่านค่าของinput analog จากขา A0
-Serial.println(sensorValue);        // ปริ๊นค่าที่ได้
-delay(100);                         //ตั้งดีเลย์ 0.1 วินาที
+  int sensorValue = analogRead(A0);   //ตั้งตัวแปล sensorValue ไว้เก็บค่าที่ได้จากการอ่านค่าของinput analog จากขา A0
+  Serial.println(sensorValue);        // ปริ๊นค่าที่ได้
+  delay(100);                         //ตั้งดีเลย์ 0.1 วินาที
 }                                   
